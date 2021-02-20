829023a335532d5cfbf80930786cbaef8c05c0cc
Andrea Undecimo <andreaundecimo.au@gmail.com>
2021-02-20 13:42:48 +0100

    feat(FE): add recipe title page component

---
+++ client/src/AddRecipeTitle/AddRecipeTitle.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AddRecipeTitle = () => {
+  return (
+    <div className="add_recipes_wrapper">
+      <h1>Hey, here you'll add the recipes!</h1>
+    </div>
+  )
+}
+
+export default AddRecipeTitle
