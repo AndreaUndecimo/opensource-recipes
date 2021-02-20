8430c20ebb1ae808a2e3b25eee1ae401429cbe1a
Andrea Undecimo <andreaundecimo.au@gmail.com>
2021-02-20 13:42:16 +0100

    style(FE): add padding

--- client/src/LandingPage/LandingPage.jsx
+++ client/src/LandingPage/LandingPage.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import './LandingPage.css'
+import { Link } from "@reach/router";
+import React from "react";
+import "./LandingPage.css";
 
 const LandingPage = () => {
   return (
     <div className="landing_wrapper">
       <div className="title_wrapper">
-      <h1>Welcome to the first open-source, editable recibe book on the Internet! </h1>
-  </div>
+        <h1>
+          Welcome to the first open-source, editable recibe book on the
+          Internet!{" "}
+        </h1>
+      </div>
       <div className="explanation">
         <h3>The rules are simple: </h3>
         <ul>
@@ -18,15 +22,17 @@ const LandingPage = () => {
       </div>
       <div className="get_started">
         <div className="get_started_title">
-        <h3>Time to get started!</h3>
+          <h3>Time to get started!</h3>
         </div>
-      <div className="get_started_btn">
-        <button>Add recipe</button>
-        <button>Browse recipe</button>
-      </div>
+        <div className="get_started_btn">
+          <Link to="/add_title">
+            <button>Add recipe</button>
+          </Link>
+          <button>Browse recipe</button>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default LandingPage
+export default LandingPage;
