dfcd62f054805bd58bde400b62b32beb1b585e60
Andrea Undecimo <andreaundecimo.au@gmail.com>
2021-02-21 09:41:06 +0100

    add(FE): bullet list for ingredients

--- client/src/AddRecipeTitle/AddRecipeTitle.jsx
+++ client/src/AddRecipeTitle/AddRecipeTitle.jsx
@@ -1,11 +1,60 @@
-import React from 'react'
+import React, { useState } from "react";
+import "./AddRecipeTitle.css";
+
+//Icons
+import { ReactComponent as PlusIcon } from "../assets/plus_icon.svg";
+import { GrRestaurant } from "react-icons/gr";
 
 const AddRecipeTitle = () => {
+  const [clicked, setClicked] = useState(false);
+
+  const toggleBullet = (e) => {
+    setClicked(!clicked);
+  };
+
   return (
     <div className="add_recipes_wrapper">
-      <h1>Hey, here you'll add the recipes!</h1>
+      <div className="h1_wrapper">
+        <h1>Hey, here you'll add the recipes!</h1>
+      </div>
+      <form className="form_recipe">
+        <label htmlFor="recipe_title">What is the title of your recipe?</label>
+        <input
+          type="text"
+          name="Recipe Title"
+          id="recipe_title"
+          placeholder="Enter the title here"
+        />
+        <label htmlFor="recipe_ingredients">What ingredients do we need?</label>
+        <div className="recipe_ingredients">
+          <div className="editable_content">
+            <button
+              onClick={(e) => toggleBullet(e)}
+              type="button"
+              className="add_btn"
+            >
+              <PlusIcon />
+            </button>
+            <div contentEditable="true" className="content_recipe"></div>
+          </div>
+          {/* {!clicked ? (
+            <button
+              onClick={(e) => toggleBullet(e)}
+              type="button"
+              className="add_btn"
+            >
+              <PlusIcon />
+            </button>
+          ) : (
+            <div className="edited_content">
+              <GrRestaurant />
+              <div contentEditable="true" className="content_recipe"></div>
+            </div>
+          )} */}
+        </div>
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default AddRecipeTitle
+export default AddRecipeTitle;
