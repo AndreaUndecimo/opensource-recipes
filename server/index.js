2560fddadb4009fdcb5ab37ffdb1e11cd5a3a950
Andrea Undecimo <andreaundecimo.au@gmail.com>
2021-02-21 21:43:05 +0100

    remove(BE): user route

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const bodyParser = require("body-parser");
 const recipeRouter = require("./routes/recipe.route");
-const userRouter = require("./routes/user.router");
 const app = express();
 
 dotenv.config();
@@ -14,7 +13,6 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/recipe", recipeRouter);
-app.use("/user", userRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
