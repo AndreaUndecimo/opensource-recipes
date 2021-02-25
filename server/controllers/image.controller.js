6148a3da53fd2415c93560be03b86e5f136cc12d
Andrea Undecimo <andreaundecimo.au@gmail.com>
2021-02-25 17:37:09 +0100

    add(BE): controller for adding image to DB

--- server/controllers/image.controller.js
+++ server/controllers/image.controller.js
@@ -5,12 +5,15 @@ const prisma = new PrismaClient();
 
 async function uploadImage(req, res, next) {
   try {
-    const fileStr = req.body.data;
-    const uploadResponse = await cloudinary.uploader.upload(fileStr, {
-      upload_preset: "bfznn6yw",
-    });
+    const { base64EncodedImage, title } = req.body.data;
+    const uploadResponse = await cloudinary.uploader.upload(
+      base64EncodedImage,
+      {
+        upload_preset: "bfznn6yw",
+      }
+    );
     const { public_id } = uploadResponse;
-    res.locals.public_id = public_id;
+    res.locals.public_id = { public_id, title };
     next();
   } catch (err) {
     console.error(err);
@@ -18,9 +21,17 @@ async function uploadImage(req, res, next) {
   }
 }
 
-async function saveImagesToDatabase(req, res) {
+async function saveImagesToDatabase(_, res) {
   try {
-    const { public_id } = res.locals;
+    const { public_id, title } = res.locals;
+    const newPic = await prisma.picture.create();
+
+    await prisma.recipe.update({
+      where: { title },
+      data: { imageIds: imageIds + public_id },
+    });
+    console.log(newPic);
+    res.status(200).send(newPic);
   } catch (error) {
     console.error(error);
   }
