128846e7ca502833e06a10023247933370987e16
Andrea Undecimo <andreaundecimo.au@gmail.com>
2021-02-25 17:35:44 +0100

    add(FE): extra recipe fields and connection images recipe

--- client/src/ApiServices/ApiClientService.js
+++ client/src/ApiServices/ApiClientService.js
@@ -1,18 +1,27 @@
 import axios from "axios";
 
-export async function postOneRecipe({ title, ingredients, name, email }) {
+export async function postOneRecipe({
+  title,
+  ingredients,
+  backgroundStory,
+  name,
+  email,
+  steps,
+}) {
   return axios.post(`http://localhost:3800/recipe/new_recipe`, {
     title,
     ingredients,
-    name,
-    email,
+    name: name || "",
+    backgroundStory: backgroundStory || "",
+    email: email || "",
+    steps,
   });
 }
 
-export async function postImageToCloudinary(base64EncodedImage) {
+export async function postImageToCloudinary({ base64EncodedImage, title }) {
   await fetch("http://localhost:3800/cloudinary/upload_image", {
     method: "POST",
-    body: JSON.stringify({ data: base64EncodedImage }),
+    body: JSON.stringify({ data: { base64EncodedImage, title } }),
     headers: { "Content-Type": "application/json" },
   });
 }
